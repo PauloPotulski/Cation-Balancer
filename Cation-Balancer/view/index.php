980305ce845413e3d2d2957822c5884fbadd9057
Paulin <ph136614@gmail.com>
2023-03-12 11:54:19 -0300

    começo da página inicial

---
+++ Cation-Balancer/view/index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"><title>Página Inicial</title>
+</head>
+<body>
+
+<!--navbar-->
+<header class="p-3 mb-3 border-bottom">
+  <div class="container">
+    <!--imagem do site-->
+    <div class="row justify-content-md-center">
+      <div class="col-md-auto">
+        <div class="d-flex  flex-wrap align-items-center justify-content-center justify-content-lg-start">
+          <a href="../view/index.php" class="d-flex align-items-center mb-2 mb-lg-0 text-dark text-decoration-none">
+            <img class="bi me-2" src="../static/img/icone.png" width="100" height="50" role="img">
+          </a>
+        <!-- tópicos da nav -->
+        <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+          <li><a href="../view/index.php" class="nav-link px-2 link-secondary">Cation Balancer</a></li>
+        </ul>
+    </div>
+  </div>
+</div>
+  </header>
+  <!-- fim da nav -->
+
+<!-- Começo do bloco de conteudo -->
+<div class="container">
+  <h1 class="text-center fw-light">Bem vindo!</h1>
+</div>
+
+<div class="container">
+    <h5 class="text-center fw-light">Vamos começar!</h5>
+</div>
+
+<div class="container overflow-hidden mt-5">
+  <div class="row gx-5">
+    <div class="col-md p-5">
+     <div class="p-3 position-relative"><a href="#" class="btn btn-danger position-absolute start-50 translate-middle">Aprender</a></div>
+    </div>
+    <div class="col-md p-5">
+      <div class="p-3 position-relative"><a href="#" class="btn btn-danger position-absolute start-50 translate-middle">Jogar</a></div>
+    </div>
+  </div>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script></html>
+</body>
+</html>
